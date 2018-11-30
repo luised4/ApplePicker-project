8aee9d5c0492c4ec438d7806f18d82f2eba4a8f2
Luis <let65@scaletmail.rutgers.edu>
2018-11-30 18:48:03 -0500

    Initial commit

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour {
+    public static float bottomY = -20f;
+	// Use this for initialization
+	
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < bottomY)
+        {
+           // print("Apple");
+            Destroy(this.gameObject);
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            apScript.AppleDestroyed();
+        }
+	}
+}
---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class HighScore : MonoBehaviour
+{
+    static public int score = 1000;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+
+        }
+        PlayerPrefs.SetInt("HighScore",score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "HightScore" + score;
+        if(score>PlayerPrefs.GetInt("High Score"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+}
